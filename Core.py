299349be5b08648c62e7663f7f5c5bf5c13444e8
McCoy Doherty <mcshaned@umich.edu>
2018-04-08 18:28:45 -0400

    Some light restructuring to reflect progress in modules

--- Core.py
+++ Core.py
@@ -1,20 +1,31 @@
-import DPSS_db_manager
-import Famous_db_manager
-import FBI_db_manager
+import DPSS_db_manager as DPSS_DB
+import Famous_db_manager as FAME_DB
+import FBI_db_manager as FBI_DB
 
-import json
-import sqlite3
+# import json
+# import sqlite3
+
 # Stack Overflow: Apparently double-import risk isn't that bad for Python
 # and it just pulls a cached version if a module has already been imported
 
-print("Core.py")
 
-def init_database():
-	try:
-		conn = sqlite3.connect("206_Final_Proj_DB")
-	except Error as e:
-		print(e)
-	cur = conn.cursor()
+print("Core.py")
+"""
+try:
+	conn = sqlite3.connect("206_Final_Proj_DB.db")
+	print("Database loaded")
+except Error as e:
+	print("Error in loading database")
+	print(e)
+"""
+
+def init_database():	
+	DPSS_DB.init_DPSS_table_prime()
+	FAME_DB.init_Famous_table_prime()
+	# Only call FAME_DB.Famous_Scrape if db is empty
+	#FAME_DB.Famous_Scrape()
+	if(FAME_DB.Famous_table_empty_check()):
+		FAME_DB.Famous_populate()
 
 
 if(__name__=="__main__"):
